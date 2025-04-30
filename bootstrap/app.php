7436968885499736dcededad58f37f7fa78c4372
Haziel Vega <hazielvega63@gmail.com>
2025-04-30 09:17:49 -0300

    nuevo grupo de rutas admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -5,6 +5,7 @@ use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Middleware\AddLinkHeadersForPreloadedAssets;
 
 return Application::configure(basePath: dirname(__DIR__))
@@ -12,6 +13,13 @@ return Application::configure(basePath: dirname(__DIR__))
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
+        // Configura rutas adicionales dentro de una función de callback.
+        then: function () {
+            Route::middleware('web', 'auth')  // Aplica los middleware 'web' y 'auth' a las rutas.
+                ->prefix('admin')  // Agrega el prefijo 'admin' a las rutas.
+                ->name('admin.')  // Asigna el prefijo 'admin.' a los nombres de las rutas.
+                ->group(base_path('routes/admin.php'));  // Carga el grupo de rutas desde 'admin.php'.
+        },
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
---
+++ routes/admin.php
@@ -0,0 +1,9 @@
+<?php
+
+use App\Http\Controllers\Admin\DashboardController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::get('/', [DashboardController::class, 'index'])
+    ->name('dashboard')
+    ->middleware('auth');
